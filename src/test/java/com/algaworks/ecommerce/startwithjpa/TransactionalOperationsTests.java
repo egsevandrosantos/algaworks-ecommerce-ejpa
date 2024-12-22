ada12fa72cb6c95ce5fa244c571d5d8a1b6a755f
Evandro Santos <egs.evandro.santos@gmail.com>
2024-12-21 23:50:48 -0300

    Inserindo o primeiro objeto com o método persist
    Using a persist method from entityManager with a transaction to insert object in database

--- src/test/java/com/algaworks/ecommerce/startwithjpa/TransactionalOperationsTests.java
+++ src/test/java/com/algaworks/ecommerce/startwithjpa/TransactionalOperationsTests.java
@@ -1,8 +1,12 @@
 package com.algaworks.ecommerce.startwithjpa;
 
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.algaworks.ecommerce.EntityManagerTests;
+import com.algaworks.ecommerce.model.Product;
 
 public class TransactionalOperationsTests extends EntityManagerTests {
 	@Test
@@ -27,5 +31,32 @@ public class TransactionalOperationsTests extends EntityManagerTests {
 		//entityManager.remove(entity);
 		
 		entityManager.getTransaction().rollback();
+	}
+	
+	@Test
+	public void insertProduct() {
+		Product expectedProduct = new Product();
+		expectedProduct.setId(2);
+		expectedProduct.setName("Camera Canon");
+		expectedProduct.setDescription("A melhor definição para suas fotos.");
+		expectedProduct.setPrice(new BigDecimal("5000.00"));
+		
+		// Out of transaction the code works because persist method wait for begin transaction
+		// But if no transaction initialize then insert query is not executed
+		// entityManager.persist(expectedProduct);
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(expectedProduct);
+		// Get unflushed objects in entityManager memory and send to database
+		// The commit transaction method execute the flush method automatically
+		// If persist and flush out of transaction an exception is thrown (TransactionRequireException)
+		// entityManager.flush();
+		entityManager.getTransaction().commit();
+		
+		// Clear memory of entityManager (to remove expectedProduct from memory and select query execute)
+		entityManager.clear();
+		
+		Product actualProduct = entityManager.find(Product.class, 2);
+		Assertions.assertEquals(expectedProduct, actualProduct);
 	}
 }
