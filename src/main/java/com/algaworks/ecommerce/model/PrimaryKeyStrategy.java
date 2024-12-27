79941ceaef0af7ee3a5d97eb9aa3e4deedc3a303
Evandro Santos <egs.evandro.santos@gmail.com>
2024-12-26 21:41:39 -0300

    Conhecendo as estratégias para geração de identificador com @GeneratedValue
    Using GenerationType.AUTO to primary key, the strategy created a sequence table with entity table name and generate id by sequence

---
+++ src/main/java/com/algaworks/ecommerce/model/PrimaryKeyStrategy.java
@@ -0,0 +1,22 @@
+package com.algaworks.ecommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "primary_key_strategies")
+public class PrimaryKeyStrategy {
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+}
---
+++ src/test/java/com/algaworks/ecommerce/startwithjpa/PrimaryKeyStrategyTests.java
@@ -0,0 +1,23 @@
+package com.algaworks.ecommerce.startwithjpa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.algaworks.ecommerce.EntityManagerTests;
+import com.algaworks.ecommerce.model.PrimaryKeyStrategy;
+
+public class PrimaryKeyStrategyTests extends EntityManagerTests {
+	@Test
+	public void testAutoStrategy() {
+		PrimaryKeyStrategy primaryKeyStrategy = new PrimaryKeyStrategy();
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(primaryKeyStrategy);
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		PrimaryKeyStrategy actualPrimaryKeyStrategy = entityManager.find(PrimaryKeyStrategy.class, primaryKeyStrategy.getId());
+		Assertions.assertEquals(primaryKeyStrategy, actualPrimaryKeyStrategy);
+	}
+}
