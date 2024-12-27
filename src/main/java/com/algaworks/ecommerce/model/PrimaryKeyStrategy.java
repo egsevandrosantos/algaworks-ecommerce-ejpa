9d54f8680c115062766bde6bb83109c71f9edb47
Evandro Santos <egs.evandro.santos@gmail.com>
2024-12-26 22:53:02 -0300

    Configurando a geração de identificador com @TableGenerator
    Using GenerationType.TABLE to primary key, the strategy created a table with management sequences by name

--- src/main/java/com/algaworks/ecommerce/model/PrimaryKeyStrategy.java
+++ src/main/java/com/algaworks/ecommerce/model/PrimaryKeyStrategy.java
@@ -4,8 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import jakarta.persistence.TableGenerator;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,7 +18,7 @@ import lombok.Setter;
 public class PrimaryKeyStrategy {
 	@EqualsAndHashCode.Include
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
-	@SequenceGenerator(name = "seq", sequenceName = "primary_key_strategies_seq", initialValue = 1, allocationSize = 50)
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = "gen")
+	@TableGenerator(name = "gen", table = "hibernate_sequences", pkColumnName = "sequence_name", pkColumnValue = "primary_key_strategy_id", valueColumnName = "next_val", initialValue = 0, allocationSize = 50)
 	private Integer id;
 }
