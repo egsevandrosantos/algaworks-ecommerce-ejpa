ed86a5711a5372eb955c7cfba0d93c82fb89721c
Evandro Santos <egs.evandro.santos@gmail.com>
2025-01-06 22:33:16 -0300

    Exercício: usando group by
    Total sales grouped by client & total sales grouped by day and category with JPQL

--- src/test/java/com/algaworks/ecommerce/jpql/GroupByTests.java
+++ src/test/java/com/algaworks/ecommerce/jpql/GroupByTests.java
@@ -1,5 +1,6 @@
 package com.algaworks.ecommerce.jpql;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
@@ -38,5 +39,59 @@ public class GroupByTests extends EntityManagerTests {
 		Assertions.assertFalse(result.isEmpty());
 		
 		result.forEach(r -> System.out.println(r[0] + "  -->  " + r[1]));
+	}
+	
+	@Test
+	public void testTotalSalesGroupedByClient() {
+		String jpql = """
+			SELECT
+				c.name,
+				SUM(o.total)
+			FROM Order o
+				JOIN o.client c
+			GROUP BY c.id
+			ORDER BY c.name
+		""";
+		
+		TypedQuery<Object[]> query = entityManager.createQuery(jpql, Object[].class);
+		
+		List<Object[]> result = query.getResultList();
+		Assertions.assertFalse(result.isEmpty());
+		
+		result.forEach(arr -> System.out.println(String.join("  -->  ", Arrays.stream(arr).map(Object::toString).toList())));
+	}
+	
+	@Test
+	public void testTotalSalesGroupedByDayAndCategory() {
+		String jpql = """
+			SELECT
+				YEAR(o.createdAt),
+				FUNCTION('monthname', o.createdAt),
+				DAY(o.createdAt),
+				c.name,
+				SUM(i.productPrice * i.quantity)
+			FROM Order o
+				JOIN o.items i
+				JOIN i.product p
+				JOIN p.categories c
+			GROUP BY
+				YEAR(o.createdAt),
+				MONTH(o.createdAt),
+				FUNCTION('monthname', o.createdAt),
+				DAY(o.createdAt),
+				c.id
+			ORDER BY
+				YEAR(o.createdAt),
+				MONTH(o.createdAt),
+				DAY(o.createdAt),
+				c.name
+		""";
+		
+		TypedQuery<Object[]> query = entityManager.createQuery(jpql, Object[].class);
+		
+		List<Object[]> result = query.getResultList();
+		Assertions.assertFalse(result.isEmpty());
+		
+		result.forEach(arr -> System.out.println(String.join("  -->  ", Arrays.stream(arr).map(Object::toString).toList())));
 	}
 }
