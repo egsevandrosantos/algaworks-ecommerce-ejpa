2819cd21435e1b8268029a7db7707b352c8c7a8b
Evandro Santos <egs.evandro.santos@gmail.com>
2025-01-07 22:42:11 -0300

    Criando subqueries
    Subqueries with jpql

---
+++ src/test/java/com/algaworks/ecommerce/jpql/SubQueriesTests.java
@@ -0,0 +1,52 @@
+package com.algaworks.ecommerce.jpql;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.algaworks.ecommerce.EntityManagerTests;
+
+import jakarta.persistence.TypedQuery;
+
+public class SubQueriesTests extends EntityManagerTests {
+	@Test
+	public void testSubQuery() {
+		// Expensive products
+		// String jpql = "SELECT MAX(p1.price) FROM Product p1"; // Get max price
+		/*String jpql = """
+			SELECT p.name, p.price
+			FROM Product p
+			WHERE p.price = (SELECT MAX(p1.price) FROM Product p1)
+		""";*/
+		
+		// Order greater than average sales
+		// String jpql = "SELECT AVG(o1.total) FROM Order o1"; // Get average
+		/*String jpql = """
+			SELECT o.total
+			FROM Order o
+			WHERE o.total > (SELECT AVG(o1.total) FROM Order o1)
+		""";*/
+		
+		// Get better clients (buy more than 20)
+		// String jpql = "SELECT c.name, SUM(o.total) FROM Order o JOIN o.client c GROUP BY c.id"; // Get total by client
+		/*String jpql = """
+			SELECT c.name
+			FROM Client c
+			WHERE (SELECT SUM(o1.total) FROM c.orders o1) > 20
+		""";*/
+		String jpql = """
+			SELECT c.name
+			FROM Client c
+			WHERE (SELECT SUM(o1.total) FROM Order o1 WHERE o1.client = c) > 20
+		""";
+		
+		TypedQuery<Object[]> query = entityManager.createQuery(jpql, Object[].class);
+		
+		List<Object[]> result = query.getResultList();
+		Assertions.assertFalse(result.isEmpty());
+		
+		result.forEach(arr -> System.out.println(String.join("  -->  ", Arrays.stream(arr).map(Object::toString).toList())));
+	}
+}
