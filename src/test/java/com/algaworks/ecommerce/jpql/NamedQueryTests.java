e9d90b690cd955ecd6426400686cce0501231123
Evandro Santos <egs.evandro.santos@gmail.com>
2025-01-12 14:22:46 -0300

    Externalizando queries em um arquivo XML
    Configuring named queries in xml file

--- src/test/java/com/algaworks/ecommerce/jpql/NamedQueryTests.java
+++ src/test/java/com/algaworks/ecommerce/jpql/NamedQueryTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.algaworks.ecommerce.EntityManagerTests;
+import com.algaworks.ecommerce.model.Order;
 import com.algaworks.ecommerce.model.Product;
 
 import jakarta.persistence.TypedQuery;
@@ -29,5 +30,24 @@ public class NamedQueryTests extends EntityManagerTests {
 		List<Product> products = query.getResultList();
 		
 		Assertions.assertFalse(products.isEmpty());
+	}
+	
+	@Test
+	public void testNamedQueryFindAllDeclaredInFile() {
+		TypedQuery<Order> query = entityManager.createNamedQuery("Order.findAll", Order.class);
+		
+		List<Order> orders = query.getResultList();
+		
+		Assertions.assertFalse(orders.isEmpty());
+	}
+	
+	@Test
+	public void testNamedQueryFindByProductDeclaredInFile() {
+		TypedQuery<Order> query = entityManager.createNamedQuery("Order.findByProduct", Order.class);
+		query.setParameter("productId", UUID.fromString("ab5666b6-3106-469b-9e34-2963b801466a"));
+		
+		List<Order> orders = query.getResultList();
+		
+		Assertions.assertFalse(orders.isEmpty());
 	}
 }
