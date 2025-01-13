383537cecfc491c7a73c3b9f2e84ce790413c80f
Evandro Santos <egs.evandro.santos@gmail.com>
2025-01-13 13:34:45 -0300

    Introdução à Criteria API do JPA
    Introduction to Criteria API

---
+++ src/test/java/com/algaworks/ecommerce/criteria/BasicCriteriaTests.java
@@ -0,0 +1,34 @@
+package com.algaworks.ecommerce.criteria;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.algaworks.ecommerce.EntityManagerTests;
+import com.algaworks.ecommerce.model.Order;
+
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+
+public class BasicCriteriaTests extends EntityManagerTests {
+	@Test
+	public void testFindById() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Order> criteriaQuery = criteriaBuilder.createQuery(Order.class);
+		Root<Order> root = criteriaQuery.from(Order.class);
+		
+		criteriaQuery.select(root); // Default (optional if use default value)
+		
+		criteriaQuery.where(criteriaBuilder.equal(root.get("id"), UUID.fromString("24be65bf-8e80-477c-81c5-277697b1bd37")));
+		
+		TypedQuery<Order> query = entityManager.createQuery(criteriaQuery);
+		
+		List<Order> orders = query.getResultList();
+		
+		Assertions.assertFalse(orders.isEmpty());
+	}
+}
