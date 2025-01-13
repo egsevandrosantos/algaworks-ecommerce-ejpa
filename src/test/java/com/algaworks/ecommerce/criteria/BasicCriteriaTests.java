fc3a0da61d3625e6718b96cb705e5bd5f2a64382
Evandro Santos <egs.evandro.santos@gmail.com>
2025-01-13 13:43:19 -0300

    Selecionando um atributo da entidade como retorno da consulta
    Select attribute from root with criteria API

--- src/test/java/com/algaworks/ecommerce/criteria/BasicCriteriaTests.java
+++ src/test/java/com/algaworks/ecommerce/criteria/BasicCriteriaTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.algaworks.ecommerce.EntityManagerTests;
+import com.algaworks.ecommerce.model.Client;
 import com.algaworks.ecommerce.model.Order;
 
 import jakarta.persistence.TypedQuery;
@@ -30,5 +31,22 @@ public class BasicCriteriaTests extends EntityManagerTests {
 		List<Order> orders = query.getResultList();
 		
 		Assertions.assertFalse(orders.isEmpty());
+	}
+	
+	@Test
+	public void testSelectClientFromOrder() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Client> criteriaQuery = criteriaBuilder.createQuery(Client.class);
+		Root<Order> root = criteriaQuery.from(Order.class); // FROM Order
+		
+		criteriaQuery.select(root.get("client")); // FROM Order SELECT client
+		
+		criteriaQuery.where(criteriaBuilder.equal(root.get("id"), UUID.fromString("24be65bf-8e80-477c-81c5-277697b1bd37")));
+		
+		TypedQuery<Client> query = entityManager.createQuery(criteriaQuery);
+		
+		List<Client> clients = query.getResultList();
+		
+		Assertions.assertFalse(clients.isEmpty());
 	}
 }
