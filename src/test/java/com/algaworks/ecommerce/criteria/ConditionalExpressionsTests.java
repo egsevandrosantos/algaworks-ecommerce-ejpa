e38f5bb1e4fe3cbdc5868ed7e4f71d951ca1f93b
Evandro Santos <egs.evandro.santos@gmail.com>
2025-01-14 22:47:56 -0300

    Usando as expressões condicionais is null e is empty
    Is null/not null and is empty/not empty with criteria API

--- src/test/java/com/algaworks/ecommerce/criteria/ConditionalExpressionsTests.java
+++ src/test/java/com/algaworks/ecommerce/criteria/ConditionalExpressionsTests.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import com.algaworks.ecommerce.EntityManagerTests;
 import com.algaworks.ecommerce.model.Client;
 import com.algaworks.ecommerce.model.Client_;
+import com.algaworks.ecommerce.model.Product;
+import com.algaworks.ecommerce.model.Product_;
 
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
@@ -30,5 +32,44 @@ public class ConditionalExpressionsTests extends EntityManagerTests {
 		List<Client> clients = query.getResultList();
 		
 		Assertions.assertFalse(clients.isEmpty());
+	}
+	
+	@Test
+	public void testIsNull() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
+		Root<Product> root = criteriaQuery.from(Product.class);
+		
+		criteriaQuery.select(root);
+		
+		// criteriaQuery.where(root.get(Product_.photo).isNull());
+		// criteriaQuery.where(root.get(Product_.photo).isNotNull());
+		
+		criteriaQuery.where(criteriaBuilder.isNull(root.get(Product_.photo)));
+		// criteriaQuery.where(criteriaBuilder.isNotNull(root.get(Product_.photo)));
+		
+		TypedQuery<Product> query = entityManager.createQuery(criteriaQuery);
+		
+		List<Product> products = query.getResultList();
+		
+		Assertions.assertFalse(products.isEmpty());
+	}
+	
+	@Test
+	public void testIsEmpty() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
+		Root<Product> root = criteriaQuery.from(Product.class);
+		
+		criteriaQuery.select(root);
+		
+		criteriaQuery.where(criteriaBuilder.isEmpty(root.get(Product_.categories)));
+		// criteriaQuery.where(criteriaBuilder.isNotEmpty(root.get(Product_.categories)));
+		
+		TypedQuery<Product> query = entityManager.createQuery(criteriaQuery);
+		
+		List<Product> products = query.getResultList();
+		
+		Assertions.assertFalse(products.isEmpty());
 	}
 }
