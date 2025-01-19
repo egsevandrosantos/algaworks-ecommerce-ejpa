4f1365c41ae1fb00643957d00218127b6dad55a4
Evandro Santos <egs.evandro.santos@gmail.com>
2025-01-19 00:59:07 -0300

    Exercício: consultando pedidos com produto específico
    Find orders with productId using Path with criteria API

--- src/test/java/com/algaworks/ecommerce/criteria/PathExpressionsTests.java
+++ src/test/java/com/algaworks/ecommerce/criteria/PathExpressionsTests.java
@@ -1,17 +1,17 @@
 package com.algaworks.ecommerce.criteria;
 
 import com.algaworks.ecommerce.EntityManagerTests;
-import com.algaworks.ecommerce.model.Client_;
-import com.algaworks.ecommerce.model.Order;
-import com.algaworks.ecommerce.model.Order_;
+import com.algaworks.ecommerce.model.*;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Root;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.UUID;
 
 public class PathExpressionsTests extends EntityManagerTests {
     @Test
@@ -29,5 +29,24 @@ public class PathExpressionsTests extends EntityManagerTests {
         TypedQuery<Order> query = entityManager.createQuery(criteriaQuery);
         List<Order> items = query.getResultList();
         Assertions.assertFalse(items.isEmpty());
+    }
+
+    @Test
+    public void testFindOrdersWithProductId() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Order> criteriaQuery = criteriaBuilder.createQuery(Order.class);
+        Root<Order> root = criteriaQuery.from(Order.class);
+        Join<Order, OrderItem> itemJoin = root.join(Order_.items);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(
+            criteriaBuilder.equal(itemJoin.get(OrderItem_.product).get(Product_.id), UUID.fromString("ab5666b6-3106-469b-9e34-2963b801466a"))
+        );
+
+        TypedQuery<Order> query = entityManager.createQuery(criteriaQuery);
+        List<Order> items = query.getResultList();
+        Assertions.assertFalse(items.isEmpty());
+        System.out.println(items.size());
     }
 }
