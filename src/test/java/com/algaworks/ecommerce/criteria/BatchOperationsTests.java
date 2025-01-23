c5d567ca79602093f20b4a42955233329baba652
Evandro Santos <egs.evandro.santos@gmail.com>
2025-01-22 23:36:34 -0300

    Batch update with criteria API

---
+++ src/test/java/com/algaworks/ecommerce/criteria/BatchOperationsTests.java
@@ -0,0 +1,46 @@
+package com.algaworks.ecommerce.criteria;
+
+import com.algaworks.ecommerce.EntityManagerTests;
+import com.algaworks.ecommerce.model.Category;
+import com.algaworks.ecommerce.model.Category_;
+import com.algaworks.ecommerce.model.Product;
+import com.algaworks.ecommerce.model.Product_;
+import jakarta.persistence.Query;
+import jakarta.persistence.criteria.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class BatchOperationsTests extends EntityManagerTests {
+    private static final BigDecimal A_HUNDRED_PER_CENT = new BigDecimal("1"); // 100 / 100
+    private static final BigDecimal TEN_PER_CENT = new BigDecimal("0.1"); // 10 / 100
+
+    @Test
+    public void testBatchUpdate() {
+        entityManager.getTransaction().begin();
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaUpdate<Product> criteriaUpdate = criteriaBuilder.createCriteriaUpdate(Product.class);
+        Root<Product> root = criteriaUpdate.from(Product.class);
+
+        criteriaUpdate.set(root.get(Product_.price), criteriaBuilder.prod(root.get(Product_.price), A_HUNDRED_PER_CENT.add(TEN_PER_CENT)));
+
+        Subquery<Integer> subquery = criteriaUpdate.subquery(Integer.class);
+        // Root<Product> rootSubquery = subquery.from(Product.class); // You can't specify target table 'products' for update in FROM clause
+        Root<Product> rootSubquery = subquery.correlate(root);
+        Join<Product, Category> joinCategorySubquery = rootSubquery.join(Product_.categories);
+        subquery.select(criteriaBuilder.literal(1));
+        subquery.where(criteriaBuilder.equal(joinCategorySubquery.get(Category_.id), UUID.fromString("d9e5d6f8-6605-4dcd-a21a-3839407a0a1f")));
+
+        criteriaUpdate.where(criteriaBuilder.exists(subquery));
+
+        Query query = entityManager.createQuery(criteriaUpdate);
+        int updated = query.executeUpdate();
+        Assertions.assertNotEquals(0, updated);
+        System.out.println(updated);
+
+        entityManager.getTransaction().commit();
+    }
+}
