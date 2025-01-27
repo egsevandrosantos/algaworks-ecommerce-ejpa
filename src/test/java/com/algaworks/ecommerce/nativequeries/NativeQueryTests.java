f09252563565cfeaa0b83abb3acb0a93a78c4cbd
Evandro Santos <egs.evandro.santos@gmail.com>
2025-01-26 22:51:17 -0300

    Executando SQL e retornando uma entidade
    Native query with typed return

--- src/test/java/com/algaworks/ecommerce/nativequeries/NativeQueryTests.java
+++ src/test/java/com/algaworks/ecommerce/nativequeries/NativeQueryTests.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.nativequeries;
 
 import com.algaworks.ecommerce.EntityManagerTests;
+import com.algaworks.ecommerce.model.Product;
 import jakarta.persistence.Query;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,11 +12,22 @@ import java.util.Objects;
 
 public class NativeQueryTests extends EntityManagerTests {
     @Test
-    public void test() {
+    public void testSelectAllFromProducts() {
         String sql = "SELECT * FROM products";
         Query query = entityManager.createNativeQuery(sql);
         List<Object[]> items = query.getResultList();
         Assertions.assertFalse(items.isEmpty());
         items.forEach(arr -> System.out.println(String.join("  -->  ", Arrays.stream(arr).map(Objects::toString).toList())));
+    }
+
+    @Test
+    public void testSelectAllFromProductsWithType() {
+        String sql = "SELECT * FROM products";
+        Query query = entityManager.createNativeQuery(sql, Product.class);
+        List<Product> items = query.getResultList();
+        Assertions.assertFalse(items.isEmpty());
+        for (Product item : items) {
+            System.out.println("Id: " + item.getId() + ", Name: " + item.getName());
+        }
     }
 }
