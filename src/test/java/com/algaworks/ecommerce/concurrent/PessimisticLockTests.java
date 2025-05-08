4acc512945f0cbe226dfe0794e6e93155a3ad6fe
Evandro Santos <egs.evandro.santos@gmail.com>
2025-05-08 12:48:06 -0300

    Fazendo Lock Pessimista com PESSIMISTIC_WRITE
    Lock with PESSIMISTIC_WRITE

--- src/test/java/com/algaworks/ecommerce/concurrent/PessimisticLockTests.java
+++ src/test/java/com/algaworks/ecommerce/concurrent/PessimisticLockTests.java
@@ -102,4 +102,72 @@ public class PessimisticLockTests {
 
         log("End test");
     }
+
+    @Test
+    public void usePessimisticWriteLockTest() throws InterruptedException {
+        Runnable run1 = () -> {
+            log("Start run 1");
+
+            String newDescription = "Detail description 1. CTM: " + System.currentTimeMillis();
+
+            try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+                entityManager.getTransaction().begin();
+
+                log("Run 1 get product kindle");
+                Product product = entityManager.find(Product.class, UUID.fromString("ab5666b6-3106-469b-9e34-2963b801466a"), LockModeType.PESSIMISTIC_WRITE);
+
+                log("Run 1 change description");
+                product.setDescription(newDescription);
+
+                log("Run 1 wait 3 seconds");
+                wait(3);
+
+                log("Run 1 commit");
+                entityManager.getTransaction().commit();
+
+                log("Run 1 end");
+            }
+        };
+
+        Runnable run2 = () -> {
+            log("Start run 2");
+
+            String newDescription = "Detail description 2. CTM: " + System.currentTimeMillis();
+
+            try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+                entityManager.getTransaction().begin();
+
+                log("Run 2 get product kindle");
+                Product product = entityManager.find(Product.class, UUID.fromString("ab5666b6-3106-469b-9e34-2963b801466a"), LockModeType.PESSIMISTIC_WRITE);
+                // If use lock PESSIMISTIC_WRITE, select wait the transaction 1 end
+                // Without the lock, update wait the transaction 1 end
+                // Because transaction 1 acquire the lock in select
+
+                log("Run 2 change description");
+                product.setDescription(newDescription);
+
+                log("Run 2 commit");
+                entityManager.getTransaction().commit();
+
+                log("Run 2 end");
+            }
+        };
+
+        Thread thread1 = new Thread(run1);
+        Thread thread2 = new Thread(run2);
+
+        thread1.start();
+        wait(1);
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            Product product = entityManager.find(Product.class, UUID.fromString("ab5666b6-3106-469b-9e34-2963b801466a"));
+            Assertions.assertTrue(product.getDescription().startsWith("Detail description 2"));
+        }
+
+        log("End test");
+    }
 }
